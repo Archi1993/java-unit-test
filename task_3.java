3c3da9d925de3f6ffa354f2fe7c8da17b998b5fd
Sortyyakov Archyn <a.sortyyakov@mail.ru>
2023-07-09 10:56:21 +0300

    My unit tests

---
+++ task_3.java
@@ -0,0 +1,38 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import static org.junit.Assert.assertEquals; // Напиши аннотацию для параметризованных тестов
+
+
+@RunWith(Parameterized.class)
+public class CheckIsAdultTest {
+
+    private final int age;
+    private final boolean result;
+
+    public CheckIsAdultTest(int age, boolean result) {
+        this.age = age;
+        this.result = result;// Инициализируй поля класса в конструкторе
+    }
+
+
+    @Parameterized.Parameters // Пометь метод аннотацией для параметров
+    public static Object[][] getTextData() {
+        return new Object[][] {
+                {17, false},
+                {18, true},
+                {19, true},
+                {21, true},// Заполни массив тестовыми данными и ожидаемым результатом
+        };
+    }
+
+
+    @Test
+    public void checkIsAdultWhenAgeThenResult() {
+        Program program = new Program();
+        boolean isAdult = program.checkIsAdult(17); // Передай сюда возраст пользователя
+        // Сравни полученный и ожидаемый результаты, не забудь про сообщение об ошибке
+        assertEquals("Должно вернуться false, так как пользователь совершеннолетний", false, isAdult);
+    }
+
+}
